918c4e07df0b05ad611a0a438baf0c1c6f048c53
ravirsk <103802048+ravirsk@users.noreply.github.com>
2023-08-15 13:35:08 -0400

    Update providers.tf

--- patterns/azure-lab/providers.tf
+++ patterns/azure-lab/providers.tf
@@ -18,6 +18,6 @@ provider "azurerm" {
   
   #use_msi = true
   subscription_id = "0d96d832-09fl-4095-ba73-c796453c9a39"
-  tennant_id = "3f0bdd77-1711-49bc-9b8c-6f2ba3e1c085"
+  tenant_id = "3f0bdd77-1711-49bc-9b8c-6f2ba3e1c085"
   #client_id = 
 }
