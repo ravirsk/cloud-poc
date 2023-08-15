f1198f30a5dd5c69de6500bf8b857af3fdeaf026
ravirsk <103802048+ravirsk@users.noreply.github.com>
2023-08-15 14:11:38 -0400

    corrected subscription id

--- patterns/azure-lab/providers.tf
+++ patterns/azure-lab/providers.tf
@@ -17,7 +17,7 @@ provider "azurerm" {
   features {}
   
   use_msi = true
-  subscription_id = "0d96d832-09fl-4095-ba73-c796453c9a39"
+  subscription_id = "0d96d832-09f1-4095-ba73-c796453c9a39"
   tenant_id = "3f0bdd77-1711-49bc-9b8c-6f2ba3e1c085"
   #client_id = 
 }
