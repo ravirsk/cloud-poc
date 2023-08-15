00b4c7ca9ed9c515213c0e78c100faf7100f77bd
ravirsk <103802048+ravirsk@users.noreply.github.com>
2023-08-15 13:37:04 -0400

    use msi

--- patterns/azure-lab/providers.tf
+++ patterns/azure-lab/providers.tf
@@ -16,7 +16,7 @@ terraform {
 provider "azurerm" {
   features {}
   
-  #use_msi = true
+  use_msi = true
   subscription_id = "0d96d832-09fl-4095-ba73-c796453c9a39"
   tenant_id = "3f0bdd77-1711-49bc-9b8c-6f2ba3e1c085"
   #client_id = 
