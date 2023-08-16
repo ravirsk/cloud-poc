64f7d934b19e0ecf215fce11887050361c3308e1
ravirsk <103802048+ravirsk@users.noreply.github.com>
2023-08-15 21:48:53 -0400

    without tennant id

--- patterns/azure-lab/providers.tf
+++ patterns/azure-lab/providers.tf
@@ -18,7 +18,7 @@ provider "azurerm" {
   
   use_msi = true
   subscription_id = "0d96d832-09f1-4095-ba73-c796453c9a39"
-  tenant_id = "3f0bdd77-1711-49bc-9b8c-6f2ba3e1c085"
+  #tenant_id = "3f0bdd77-1711-49bc-9b8c-6f2ba3e1c085"
   #client_id = "f46e2ba7-84ff-49ae-a4e7-cbe4964685db"
   #client id for MI harnessdelegateid
 }
