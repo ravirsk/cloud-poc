c0b2bf7f0894fb121b57c990fbf57a799323c306
ravirsk <103802048+ravirsk@users.noreply.github.com>
2023-08-16 13:54:29 -0400

    corrected subnet IP

--- patterns/azure-lab/main.tf
+++ patterns/azure-lab/main.tf
@@ -16,7 +16,7 @@ resource "azurerm_subnet" "my_terraform_subnet" {
   name                 = "ssc-core-ea-innolab-14-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.my_terraform_network.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["172.20.0.0/24"]
 }
 
 # Create public IPs
