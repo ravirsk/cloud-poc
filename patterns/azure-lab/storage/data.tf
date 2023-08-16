90c59514abd1596831a7239005dbe4c6ed9b8dc0
ravirsk <103802048+ravirsk@users.noreply.github.com>
2023-08-16 17:06:05 -0400

    removed subnet from data

--- patterns/azure-lab/storage/data.tf
+++ patterns/azure-lab/storage/data.tf
@@ -1,9 +1,3 @@
 data "azurerm_resource_group"  "rg14" {
 	name = "ssc-core-ea-innolab-rg-14"
 } 
-
-data "azurerm_subnet" "example" {
-  name                 = "rg-14-default-01"
-  virtual_network_name = "ssc-core-ea-innolab-rg-14-vnet"
-  resource_group_name  = "ssc-core-ea-innolab-rg-14"
-}
