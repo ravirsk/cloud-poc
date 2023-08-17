e267fcfe070c7529673882e7e6599a1f00b5614f
ravirsk <103802048+ravirsk@users.noreply.github.com>
2023-08-17 10:04:31 -0400

    Update main.tf

--- patterns/azure-lab/storage/main.tf
+++ patterns/azure-lab/storage/main.tf
@@ -3,9 +3,12 @@ terraform {
 		resource_group_name	= "ssc-ccoe-ea-innolab-rg-14"
 		storage_account_name = "tfblobstateacct"
 		container_name 		= "blobstatecontainer"
-		key					= "terraform-state-storeage-blob.tfstate"
-	}
+		key					= "lab.terraform-storeage-blob.tfstate"
 
+		use_msi = true
+ 		subscription_id = "0d96d832-09f1-4095-ba73-c796453c9a39"
+		tenant_id = "3f0bdd77-1711-49bc-9b8c-6f2ba3e1c085"
+	}
 }
 
 # Create storage account
