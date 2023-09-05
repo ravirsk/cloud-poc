784534fbbe7fa8a9b87ef260e5dbd60af2a1f4df
ravirsk <103802048+ravirsk@users.noreply.github.com>
2023-09-05 16:36:14 -0400

    Update user-data.sh

--- patterns/lab/ec2/user-data.sh
+++ patterns/lab/ec2/user-data.sh
@@ -7,10 +7,12 @@ sudo systemctl enable httpd
 sudo chmod 777 /var/www/html
 echo "<h1>Hello, World From Standalone EC2 </h1>" >> /var/www/html/index.html
 echo "<h1>I am not load balanced : $ {instance_desc} </h1>" >> /var/www/html/index.html
-echo "Installing My Spring Boot App" >> logs.txt
-sudo yum install java-17-amazon-corretto >> logs.txt
-echo "Installed JAVA" >> logs.txt 
-wget https://cloudarchitect.jfrog.io/artifactory/libs-release/student-services-security-0.0.1-SNAPSHOT.jar >> logs.txt
-sudo chmod 777 student-services-security-0.0.1-SNAPSHOT.jar >> logs.txt
-echo "Downloaded Jar" >> logs.txt 
-java -jar -Dserver.port=8081 student-services-security-0.0.1-SNAPSHOT.jar com.in28minutes.springboot.StudentServicesApplication & >> logs.txt
+sudo mkdir -p /home/app_data
+sudo chmod 777 /home/app_data
+echo "Installing My Spring Boot App" >> /home/app_data/appinstalllog.txt
+sudo yum install java-17-amazon-corretto >> /home/app_data/appinstalllog.txt
+echo "Installed JAVA" >> /home/app_data/appinstalllog.txt 
+wget https://cloudarchitect.jfrog.io/artifactory/libs-release/student-services-security-0.0.1-SNAPSHOT.jar >> /home/app_data/appinstalllog.txt
+sudo chmod 777 student-services-security-0.0.1-SNAPSHOT.jar >> /home/app_data/appinstalllog.txt
+echo "Downloaded Jar" >> /home/app_data/appinstalllog.txt 
+java -jar -Dserver.port=8081 student-services-security-0.0.1-SNAPSHOT.jar com.in28minutes.springboot.StudentServicesApplication & >> /home/app_data/appinstalllog.txt
