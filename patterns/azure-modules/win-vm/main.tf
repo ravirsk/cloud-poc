967ffdf2d271ba49b2af62f69c0c82f8580b164a
ravirsk <103802048+ravirsk@users.noreply.github.com>
2023-09-13 23:55:11 -0400

    Update main.tf

--- patterns/azure-modules/win-vm/main.tf
+++ patterns/azure-modules/win-vm/main.tf
@@ -137,9 +137,11 @@ resource "azurerm_windows_virtual_machine" "main" {
 resource "azurerm_virtual_machine_extension" "web_server_install" {
 	name                       = "${var.prefix}-wsi"
 	virtual_machine_id         = azurerm_windows_virtual_machine.main.id
-	publisher                  = "Microsoft.Compute"
-	type                       = "CustomScriptExtension"
-	type_handler_version       = "1.8"
+	#publisher                  = "Microsoft.Compute"
+	publisher                  = "Microsoft.Powershell"
+	#type                       = "CustomScriptExtension"
+	type                       = "DSC"	
+	type_handler_version       = "2.77"
 	auto_upgrade_minor_version = true
 
 	settings = <<SETTINGS
