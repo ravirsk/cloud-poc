ef684296edf1274f6d012b5f5f4311747c01a592
ravirsk <103802048+ravirsk@users.noreply.github.com>
2023-09-21 15:23:47 -0400

    using Azure publisher

--- patterns/azure-modules/win-vm/main.tf
+++ patterns/azure-modules/win-vm/main.tf
@@ -137,11 +137,13 @@ resource "azurerm_windows_virtual_machine" "main" {
 resource "azurerm_virtual_machine_extension" "web_server_install" {
 	name                       = "${var.prefix}-wsi"
 	virtual_machine_id         = azurerm_windows_virtual_machine.main.id
-	#publisher                  = "Microsoft.Compute"
-	publisher                  = "Microsoft.Powershell"
-	#type                       = "CustomScriptExtension"
-	type                       = "DSC"	
-	type_handler_version       = "2.77"
+	publisher                  = "Microsoft.Azure.Extensions"
+	#publisher                 = "Microsoft.Compute"
+	#publisher                 = "Microsoft.Powershell"
+	type                       = "CustomScript"
+	#type                      = "CustomScriptExtension"
+	#type                      = "DSC"	
+	type_handler_version       = "2.0"
 	auto_upgrade_minor_version = true
 
 	settings = <<SETTINGS
@@ -156,6 +158,7 @@ resource "azurerm_virtual_machine_extension" "web_server_install" {
 	}
 	timeouts {
 		create =  "1h30m"
+		update =  "1h30m"
 		delete =  "20m"
 	}
 
